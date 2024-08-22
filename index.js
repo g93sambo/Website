1872023ec8d2dff61447c2d3baa89da1e735dd32
g93sambo <adetunlesearowolo@gmail.com>
2024-08-22 10:32:55 +0100

    library

--- index.js
+++ index.js
@@ -1,26 +1,72 @@
+const modal = document.querySelector("dialog");
+const addBtn = document.querySelector("#add-button");
+const closeBtn = document.querySelector("#close");
+const bookContainer = document.querySelector(".grid");
+const addBtnContainer = document.querySelector(".add-book");
 
+const form = document.querySelector("form");
 
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#page-number");
+const readInput = document.querySelector("#read");
 
+const libraryBooks = [];
 
-function playRound(HumanChoice) {
-    let humanScore = 0;
-    let computerScore = 0;
-    let result = "";
-    const choices = ['rock', 'paper', 'scissors'];
-    const ComputerChoice = choices[Math.floor(Math.random() * choices.length)];
-    if (HumanChoice === ComputerChoice) {
-        result = "Its a tie"
-    } else if ((HumanChoice === 'paper' && ComputerChoice === 'rock') ||
-        (HumanChoice === 'rock' && ComputerChoice === 'scissors') ||
-        (HumanChoice === 'scissors' && ComputerChoice === 'paper')) {
-        result = "player wins this round!";
-        humanScore++;
-    } else {
-        result = "computer wins this round!";
-        computerScore++;
-    }
-    const div = document.querySelector("div");
-    div.innerText = result;
-    document.getElementById('pop').innerText = `humanScore: ${humanScore}, computerScore: ${computerScore}`;
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
+addBtn.addEventListener("click", e =>{
+    modal.showModal();
+});
+
+closeBtn.addEventListener("click", 
+    e =>{
+        modal.close();
+        e.preventDefault();
+    }
+)
+
+form.addEventListener("submit", e => {
+        const book = new Book(titleInput.value, authorInput.value,
+                              pagesInput.value, readInput.checked);  
+        libraryBooks.push(book);
+        showBooks(book, libraryBooks.length - 1);
+        form.reset();
+    }
+);
+
+function showBooks(bookobj, index){
+    const book = document.createElement("div")
+    book.className = "book"
+    book.setAttribute("data-index", `${index}`)
+
+    const titleele = document.createElement("h2")
+    titleele.textContent = `${bookobj.title}`
+
+    const authorele = document.createElement("h3")
+    authorele.textContent = `${bookobj.author}`
+
+    const pagesele = document.createElement("h4")
+    pagesele.textContent = `${bookobj.pages}  "pages"`
+
+    const readele = document.createElement("button")
+    if(bookobj.read){
+        readele.textContent = "Read"
+        readele.className = "read"
+    } else {
+        readele.textContent = "Not read"
+        readele.className = "not-read"
+    }
+
+   book.appendChild(titleele)
+   book.appendChild(authorele)
+   book.appendChild(pagesele)
+   book.appendChild(readele)
+
+   bookContainer.insertBefore(book, addBtnContainer)
+}
